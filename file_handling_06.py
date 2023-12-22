39443fc77b0af79cf87ad819f6f4ff0dbcb2f388
salino3 <programacionflavio@gmail.com>
2023-12-22 12:44:14 +0100

    commit

--- file_handling_06.py
+++ file_handling_06.py
@@ -3,13 +3,42 @@ import os
 
 # .txt tile
 
-txt_file = open("Desktop/Python/python-intermedio/my_file.txt", "r")
+txt_file = open("./my_file.txt", "w+")
+txt_file.write("My name is Giovanni\nMy surname is Doe\n45 years\nI'm workin with Python")
 
 
-print(txt_file.read())
+# print(txt_file.read())
+
+# 
+
+print(txt_file.read(10))
+
+# 
+
+print(txt_file.readline())
+print(txt_file.readline())
+
+# 
+
+# print(txt_file.readlines())
+
+# 
+
+for line in txt_file.readlines():
+    print(line)
+
+# 
+
+txt_file.write("\n\nBut yet I like Kotlin")
+
+txt_file.close()
+
+# for delete the file
+# os.remove("./my_file.txt")
 
 
-####
+
+#### Other manner for read file .txt
 
 # import os
 
